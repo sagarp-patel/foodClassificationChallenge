ad4851bce29e4e97c5421e44673ce7f2ab398ad6
Sagar Patel <sagarp@coyote.csusb.edu>
2020-01-21 14:45:54 -0800

    Starting on Resnet Model

--- Food_Classification/model.py
+++ Food_Classification/model.py
@@ -5,3 +5,9 @@ Created on Tue Jan 14 12:58:18 2020
 @author: sagar
 This is where the model will be created
 """
+import numpy as np
+from keras.preprocessing.image import image
+from keras.preprocessing.image import img_to_array
+from keras.applications.resnet50 import preprocess_input
+from keras.applications.image_net_utils import decode_predictions
+import matplotlib.pyplot as pyplt
